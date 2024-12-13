c23efd187e2d5741be2357d3f3ace2394227f535
Gavin Rhoades <84148006+SithLor@users.noreply.github.com>
2024-12-13 12:02:03 -0800

    remove files

--- test.ts
+++
@@ -1,9 +0,0 @@
-//scale 10 
-
-//const t = [1,2,3,4,5,6,7,8,9,10];
-//
-////scale ecah element by by the value befor it
-//for (let i = 1; i < t.length; i++) {
-//    t[i] = t[i] * t[i - 1];
-//}
-const t = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; for (let i = 1; i < t.length; i++)t[i] = t[i] * t[i - 1];
